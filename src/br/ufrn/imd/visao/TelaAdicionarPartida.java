7c8100d54ef52df11e4127e7763ae6de979443d9
arturo32 <arturofonsecas32@gmail.com>
2021-08-29 00:22:15 -0300

    Begin development of "Adcionar partida" screen

--- src/br/ufrn/imd/visao/TelaAdicionarPartida.java
+++ src/br/ufrn/imd/visao/TelaAdicionarPartida.java
@@ -9,15 +9,21 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.text.NumberFormatter;
 
 import br.ufrn.imd.controle.BancoSelecao;
 import br.ufrn.imd.modelo.esportes.Esporte;
@@ -27,8 +33,13 @@ public class TelaAdicionarPartida extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
 	int codF = 1;
 	
+	JPanel panelCardCentral;
 	JComboBox<String> paises1;
 	JComboBox<String> paises2;
+	JFormattedTextField placarParticipante1Futebol;
+	JFormattedTextField placarParticipante2Futebol;
+	private int testeInt = 10;
+	
 	
 	public TelaAdicionarPartida(Esporte esporte) {
 		Container ct = this.getContentPane();
@@ -54,23 +65,14 @@ public class TelaAdicionarPartida extends JFrame implements ActionListener{
 		JPanel panelBoxCentral = new JPanel();
 		panelBoxCentral.setLayout(new BorderLayout());
 		
-		JPanel panelCardCentral = new JPanel();
+		panelCardCentral = new JPanel();
 		panelCardCentral.setLayout(new CardLayout());
 		
-		JPanel panelSelectsPaises = new JPanel();
-		panelSelectsPaises.setLayout(new BoxLayout(panelSelectsPaises, BoxLayout.LINE_AXIS));
-		//panelSelectsPaises.setLayout(new FlowLayout());
-		panelSelectsPaises.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
-		panelSelectsPaises.add(Box.createVerticalGlue());
-		paises1.setMaximumSize(new Dimension(400, 30));
-		paises2.setMaximumSize(new Dimension(400, 30));
-		panelSelectsPaises.add(paises1);
-		JLabel labelX = new JLabel("X");
-		labelX.setBorder(BorderFactory.createEmptyBorder(0, 30, 0, 30));
-		panelSelectsPaises.add(labelX);
-		panelSelectsPaises.add(paises2);
 		
-		panelCardCentral.add(panelSelectsPaises);
+		
+		panelCardCentral.add(etapaPaises(), "Teste");
+		panelCardCentral.add(etapaPartida(), "Teste2");
+		
 		panelBoxCentral.add(panelCardCentral, BorderLayout.CENTER);
 		
 		
@@ -106,10 +108,53 @@ public class TelaAdicionarPartida extends JFrame implements ActionListener{
 		setTitle("Adicionar partida de " + esporte);
 	}
 
+	
+	private JPanel etapaPaises() {
+		JPanel panelSelectsPaises = new JPanel();
+		panelSelectsPaises.setLayout(new BoxLayout(panelSelectsPaises, BoxLayout.LINE_AXIS));
+		panelSelectsPaises.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
+		panelSelectsPaises.add(Box.createVerticalGlue());
+		paises1.setMaximumSize(new Dimension(400, 30));
+		paises2.setMaximumSize(new Dimension(400, 30));
+		panelSelectsPaises.add(paises1);
+		JLabel labelX = new JLabel("X");
+		labelX.setBorder(BorderFactory.createEmptyBorder(0, 30, 0, 30));
+		panelSelectsPaises.add(labelX);
+		panelSelectsPaises.add(paises2);
+		return panelSelectsPaises;
+	}
+	
+	private JPanel etapaPartida() {
+		JPanel panelPartida = new JPanel();
+		panelPartida.setLayout(new BoxLayout(panelPartida, BoxLayout.LINE_AXIS));
+		JLabel labelX = new JLabel("X");
+		
+
+		
+		NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.getDefault());
+		DecimalFormat decimalFormat = (DecimalFormat) numberFormat;
+		decimalFormat.setGroupingUsed(false);
+		placarParticipante1Futebol = new JFormattedTextField(decimalFormat);
+		placarParticipante1Futebol.setColumns(15); 
+		
+		panelPartida.add(placarParticipante1Futebol);
+		placarParticipante1Futebol.setMaximumSize(new Dimension(400, 30));
+		
+		panelPartida.add(labelX);
+		
+		placarParticipante2Futebol = new JFormattedTextField();
+		placarParticipante2Futebol.setMaximumSize(new Dimension(400, 30));
+		panelPartida.add(placarParticipante2Futebol);
+		
+		
+		return panelPartida;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getActionCommand().equals("Selecionar")) {
-			
+			CardLayout cl = (CardLayout)(panelCardCentral.getLayout());
+	        cl.show(panelCardCentral, "Teste2");
 		}
 		else if(e.getActionCommand().equals("Cancelar")) {
 			this.dispose();
