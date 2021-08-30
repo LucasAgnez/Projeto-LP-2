6952d3803f541181ff3cd1a1dbdbfc74208442a9
arturo32 <arturofonsecas32@gmail.com>
2021-08-30 08:08:57 -0300

    Improve initial screen

--- src/br/ufrn/imd/visao/TelaPrincipal.java
+++ src/br/ufrn/imd/visao/TelaPrincipal.java
@@ -1,16 +1,18 @@
 package br.ufrn.imd.visao;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.GraphicsEnvironment;
-import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 
 
 
@@ -19,41 +21,47 @@ public class TelaPrincipal extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 
-	private JMenuBar mnbar = new JMenuBar();
-	private JMenu menuAjud = new JMenu("Ajuda");
 
 	public TelaPrincipal() {
 		Container ct = this.getContentPane();
-		ct.setLayout((new GridLayout(3,1)));
+		ct.setLayout((new BorderLayout()));
 		
-		setJMenuBar(mnbar);
-		mnbar.add(menuAjud);
 
+		JPanel panelBox = new JPanel();
+		panelBox.setLayout(new BoxLayout(panelBox, BoxLayout.PAGE_AXIS));
 		
 		JButton buttonAdicionarPartida = new JButton("Adicionar partida");
+		buttonAdicionarPartida.setMaximumSize(new Dimension(250, 40));
 		buttonAdicionarPartida.setActionCommand("Adicionar");
 		buttonAdicionarPartida.addActionListener(this);	
-		//buttonAdicionarPartida.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		buttonAdicionarPartida.setAlignmentX(CENTER_ALIGNMENT);
 
 		JButton buttonBuscarPartida = new JButton("Buscar Partidas");
+		buttonBuscarPartida.setMaximumSize(new Dimension(250, 40));
 		buttonBuscarPartida.setActionCommand("Buscar Partida");
 		buttonBuscarPartida.addActionListener(this);
 		buttonBuscarPartida.setAlignmentX(CENTER_ALIGNMENT);
 		
 		JButton buttonBuscarAtleta= new JButton("Buscar Atletas");
+		buttonBuscarAtleta.setMaximumSize(new Dimension(250, 40));
 		buttonBuscarAtleta.setActionCommand("Buscar Atleta");
 		buttonBuscarAtleta.addActionListener(this);
 		buttonBuscarAtleta.setAlignmentX(CENTER_ALIGNMENT);
 		
 		//Adicionando todos paineis ao container principal
-		ct.add(buttonBuscarPartida);
-		ct.add(buttonAdicionarPartida);
-		ct.add(buttonBuscarAtleta);
+		panelBox.add(Box.createRigidArea(new Dimension(0, 150)));
+		panelBox.add(buttonBuscarPartida);
+		panelBox.add(Box.createRigidArea(new Dimension(0, 20)));
+		panelBox.add(buttonAdicionarPartida);
+		panelBox.add(Box.createRigidArea(new Dimension(0, 20)));
+		panelBox.add(buttonBuscarAtleta);
+		panelBox.add(Box.createRigidArea(new Dimension(0, 20)));
+		
+		ct.add(panelBox, BorderLayout.CENTER);
 		
 		//Configurando tamanho, posição e título da janela
-        int larguraJanela = 1200;
-        int alturaJanela = 625;
+        int larguraJanela = 500;
+        int alturaJanela = 500;
 		setSize(larguraJanela, alturaJanela);
 		Point centro = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
 		setBounds(centro.x - larguraJanela / 2, centro.y - alturaJanela / 2, larguraJanela, alturaJanela);
